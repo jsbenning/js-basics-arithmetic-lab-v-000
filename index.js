b8886b9372c3817b81bd1201704f45f8a74945c6
Josef Benninghoff <jsbenning4@gmail.com>
2018-05-01 01:21:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,4 @@ let newID = oldID + 1000000000;
 let ageIsValid = Number.isInteger(currentAge);
 let randomNumber = Math.floor(Math.random() * 20 + 1);
 let randomInteger = Math.floor(randomNumber);
+let randomID = randomInteger + 
